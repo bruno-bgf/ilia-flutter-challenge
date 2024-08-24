51a9cd8bd3c58e52e81a58f5a7fcdb548b5c39c9
bruno-bgf <brunobgf30@gmail.com>
2024-08-24 08:54:31 -0300

    feat: created app structure and themes

---
+++ movie_app/lib/src/themes/app_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/src/themes/movie_app_colors.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    primaryColor: MovieHubColors.primaryPurple,
+    scaffoldBackgroundColor: MovieHubColors.cultured,
+    brightness: Brightness.light,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: MovieHubColors.primaryPurple,
+        textStyle: const TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30),
+        ),
+      ),
+    ),
+  );
+}
---
+++ movie_app/lib/src/themes/movie_app_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieHubColors {
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color transparent = Color(0x00000000);
+  static const Color lightGrey = Color(0xFFE3E6E9);
+  static const Color cultured = Color(0xFFEFF2F5);
+  static const Color primaryPurple = Color(0xFF8000FF);
+  static const Color secondaryPurple = Color(0xFF540BA1);
+  static const Color darkRed = Color(0xFFAF1F1A);
+  static const Color graniteGray = Color(0xFF666666);
+
+  const MovieHubColors();
+}
